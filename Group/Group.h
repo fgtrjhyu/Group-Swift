84f93427f7b14b2414277207a4c57ddb64133f9f
fgtrjhyu <root@localhost>
2015-04-06 09:52:09 +0900

    Initial Commit

---
+++ Group/Group.h
@@ -0,0 +1,19 @@
+//
+//  Group.h
+//  Group
+//
+//  Created by 江口 隆夫 on 2015/04/06.
+//  Copyright (c) 2015年 fgtrjhyu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Group.
+FOUNDATION_EXPORT double GroupVersionNumber;
+
+//! Project version string for Group.
+FOUNDATION_EXPORT const unsigned char GroupVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Group/PublicHeader.h>
+
+
---
+++ GroupTests/GroupTests.swift
@@ -0,0 +1,36 @@
+//
+//  GroupTests.swift
+//  GroupTests
+//
+//  Created by 江口 隆夫 on 2015/04/06.
+//  Copyright (c) 2015年 fgtrjhyu. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class GroupTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
